c083a5dd17eee648f36e39601c7e7ce45d9c28e2
YuvanX <abhivigneshofficial@gmail.com>
2025-06-02 19:20:46 +0530

    feat: Add HeroSection

---
+++ components/ui/hero-section.tsx
@@ -0,0 +1,17 @@
+export const HeroSection = () => {
+  return (
+    <div className="min-h-screen">
+        <div className="flex justify-between items-center text-black py-5 px-40">
+            <div className="font-fk-display text-4xl font-extrabold">
+                unfold
+            </div>
+            <div className="uppercase text-xl font-medium">
+                Let's chat
+            </div>
+        </div>
+      <div className="inline-flex font-fk-display text-[250px]/55 font-medium  text-black px-20">
+        <div>A collective that builds&#44; designs&#44; and explores.</div>
+      </div>
+    </div>
+  );
+};
---
+++ components/ui/navbar.tsx
@@ -0,0 +1,10 @@
+export const NavBar = () => {
+  return (
+    <div className="inline-flex uppercase  gap-10 bg-white px-10 py-5 text-xl rounded-full text-black fixed bottom-10 left-1/2 -translate-x-1/2 cursor-pointer font-fk-display font-medium">
+      <div>home</div>
+      <div>work</div>
+      <div>about</div>
+      <div>contact</div>
+    </div>
+  );
+};
