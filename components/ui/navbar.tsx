e9eb3b4062726224e2c9954de465efcc400e8f04
YuvanX <abhivigneshofficial@gmail.com>
2025-06-08 10:24:27 +0530

    feat: Animate NavBar

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -1,10 +1,26 @@
+"use client";
+import { useState } from "react";
+import { motion } from "framer-motion";
+
 export const NavBar = () => {
+  const TABS = ["Home", "Work", "About", "Contact"];
+  const [selectedTab, setSelectedTab] = useState(TABS[0]);
+
   return (
-    <div className="inline-flex uppercase gap-10 bg-white px-10 py-5 text-xl rounded-full text-black fixed bottom-10 left-1/2 -translate-x-1/2 cursor-pointer font-fk-display font-medium shadow-lg z-10">
-      <div>home</div>
-      <div>work</div>
-      <div>about</div>
-      <div>contact</div>
+    <div className="inline-flex uppercase gap-10 bg-white px-5 py-3 text-xl rounded-full text-black fixed bottom-10 left-1/2 -translate-x-1/2 font-fk-display font-medium shadow-lg z-10 items-center">
+      {TABS.map((t, i) => (
+        <div
+          onClick={() => setSelectedTab(t)}
+          key={i}
+          className="relative px-8 py-2 cursor-pointer"
+        >
+          {selectedTab === t && (
+            <motion.div layoutId="current-tab" className="absolute inset-0 rounded-full bg-black" />
+          )}
+
+          <span className={`relative ${selectedTab === t ? "text-white" : "text-black"} z-50`}>{t}</span>
+        </div>
+      ))}
     </div>
   );
 };
