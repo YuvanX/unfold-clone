d79ff8f86252a72a64a00ada904605e1eb58b9a7
YuvanX <abhivigneshofficial@gmail.com>
2025-06-08 11:26:47 +0530

    feat: Add video in heroSection

--- components/ui/hero-section.tsx
+++ components/ui/hero-section.tsx
@@ -7,12 +7,22 @@ export const HeroSection = () => {
           alt="unfold_logo"
           className="w-44"
         />
-        <div className="uppercase text-lg  font-medium bg-[#F6FA5E]   rounded-b-full w-56 cursor-pointer font-fk-display flex justify-center items-center h-28 mr-20">
+        <div className="uppercase text-lg  font-medium bg-[#F6FA5E]   rounded-b-full w-56 cursor-pointer font-fk-display flex justify-center items-center h-28 mr-10">
           Let's chat
         </div>
       </div>
-      <div className="inline-flex font-fk-display text-[250px]/55 font-medium  text-black  tracking-tighter py-20">
-        <div>A collective that builds&#44; designs&#44; and explores.</div>
+      <div className="inline-flex font-fk-display text-[264px]/55 font-medium  text-black  tracking-tighter py-20">
+        <div>
+          A collective that builds&#44; designs&#44;
+          <span className="inline-block align-middle w-[550px] h-[250px] pt-1.5 ml-8 my-2">
+            <video
+              className="w-full h-full object-cover rounded-full"
+              src="https://cdn.prod.website-files.com/63dae19244d432799bf5baa4%2F6657fa4d1432e66c43c27103_tK80bh-transcode.mp4"  loop autoPlay
+            > </video>
+           
+          </span>
+          and explores.
+        </div>
       </div>
     </div>
   );
