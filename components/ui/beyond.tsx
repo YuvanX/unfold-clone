0d0243683667c748ef7b882d5a9404a47d17cb90
YuvanX <abhivigneshofficial@gmail.com>
2025-06-09 22:27:12 +0530

    fix: Make BeyoundSection Responsive

--- components/ui/beyond.tsx
+++ components/ui/beyond.tsx
@@ -1,12 +1,12 @@
 export const Beyond = () => {
   return (
-    <div className="flex items-center justify-between text-black pb-20 px-20">
-      <div className="max-w-sm text-xl font-fk-grotesk font-medium">
+    <div className="flex flex-col-reverse xl:flex-row items-center justify-between text-black  mx-5 xl:mx-20 my-10 xl:my-32">
+      <div className="max-w-sm text-[17px]/5 xl:text-xl text-center font-fk-grotesk font-medium">
         Your brand isn’t an aesthetic. It’s a living organism. We help it thrive
         by bringing a dynamic perspective and collaborative expertise to
         everything we build.
       </div>
-      <div className="font-fk-display text-9xl max-w-xl text-right">Beyond Design</div>
+      <div className="font-fk-display text-3xl xl:text-9xl max-w-xl text-right mb-3">Beyond Design</div>
     </div>
   );
 };
