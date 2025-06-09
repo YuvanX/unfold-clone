34d369fe6883cd293674d55b5abb14dca14ebfe8
YuvanX <abhivigneshofficial@gmail.com>
2025-06-09 22:07:54 +0530

    fix: Make JawSection Reponsive

--- components/ui/jaw.tsx
+++ components/ui/jaw.tsx
@@ -1,32 +1,32 @@
 export const Jaw = () => {
-    return <div className="h-screen">
-        <div className="mt-20 p-24 bg-white text-black rounded-[40px] min-h-[80vh] mx-20">
-            <img src="https://cdn.prod.website-files.com/63dae19244d432799bf5baa4/665114658293e25fc21ed174_jamm-logo.svg" alt="jaw" width={350}/>
-            <div className="flex justify-between items-end">
-                <div className="text-7xl font-fk-display font-medium max-w-sm mt-10 leading-20">
+    return <div className="xl:h-screen">
+        <div className="mx-4 p-5 min-h-[400px] xl:min-h-[80vh] bg-white text-black rounded-xl xl:rounded-[40px]  xl:mx-20 xl:mt-20 xl:p-24">
+            <img src="https://cdn.prod.website-files.com/63dae19244d432799bf5baa4/665114658293e25fc21ed174_jamm-logo.svg" alt="jaw" className="xl:w-[350px] w-[200px]"/>
+            <div className="flex flex-col xl:flex-row justify-between items-end">
+                <div className="text-[23px]/6 xl:text-7xl font-fk-display font-semibold xl:font-medium max-w-sm mt-10 xl:leading-20">
                     Unfold's subscription service
                 </div>
-                <div className="flex gap-x-32 font-fk-display">
-                    <div className="flex flex-col">
-                        <div className="capitalize mt-2 text-2xl">branding</div>
-                        <div className="capitalize mt-2 text-2xl">print</div>
-                        <div className="capitalize mt-2 text-2xl">stratery</div>
-                        <div className="capitalize mt-2 text-2xl">logos</div>
-                        <div className="capitalize mt-2 text-2xl">consulting</div>
+                <div className="grid grid-cols-2 xl:grid-cols-3 xl:gap-x-32 font-fk-display w-full md:w-1/2 mt-5">
+                    <div >
+                        <div className="capitalize leading-6 text-lg xl:leading-12 xl:text-2xl">branding</div>
+                        <div className="capitalize leading-6 text-lg xl:leading-12 xl:text-2xl">print</div>
+                        <div className="capitalize leading-6 text-lg xl:leading-12 xl:text-2xl">stratery</div>
+                        <div className="capitalize leading-6 text-lg xl:leading-12 xl:text-2xl">logos</div>
+                        <div className="capitalize leading-6 text-lg xl:leading-12 xl:text-2xl">consulting</div>
                     </div>
-                      <div className="flex flex-col">
-                        <div className="capitalize mt-2 text-2xl">webdesign</div>
-                        <div className="capitalize mt-2 text-2xl">illustration</div>
-                        <div className="capitalize mt-2 text-2xl">copywritring</div>
-                        <div className="capitalize mt-2 text-2xl">development</div>
-                        <div className="uppercase mt-2 text-2xl">ui/ux</div>
+                      <div >
+                        <div className="capitalize leading-6 text-lg xl:leading-12 xl:text-2xl">webdesign</div>
+                        <div className="capitalize leading-6 text-lg xl:leading-12 xl:text-2xl">illustration</div>
+                        <div className="capitalize leading-6 text-lg xl:leading-12 xl:text-2xl">copywritring</div>
+                        <div className="capitalize leading-6 text-lg xl:leading-12 xl:text-2xl">development</div>
+                        <div className="uppercase leading-6 text-lg xl:leading-12 xl:text-2xl">ui/ux</div>
                     </div>
-                      <div className="flex flex-col">
-                        <div className="capitalize mt-2 text-2xl">Product</div>
-                        <div className="capitalize mt-2 text-2xl">apps</div>
-                        <div className="capitalize mt-2 text-2xl">icons</div>
-                        <div className="capitalize mt-2 text-2xl">design systems</div>
-                        <div className="capitalize mt-2 text-2xl">prototyping</div>
+                      <div >
+                        <div className="capitalize leading-6 text-lg xl:leading-12 xl:text-2xl">Product</div>
+                        <div className="capitalize leading-6 text-lg xl:leading-12 xl:text-2xl">apps</div>
+                        <div className="capitalize leading-6 text-lg xl:leading-12 xl:text-2xl">icons</div>
+                        <div className="capitalize leading-6 text-lg xl:leading-12 xl:text-2xl">design systems</div>
+                        <div className="capitalize leading-6 text-lg xl:leading-12 xl:text-2xl">prototyping</div>
                     </div>
 
                 </div>
