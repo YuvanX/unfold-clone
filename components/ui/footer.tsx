eb55c01a7ffd5534990e09bb33cbfe85c3f2f05c
YuvanX <abhivigneshofficial@gmail.com>
2025-06-10 07:13:22 +0530

    fix: Make Footer responsive

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <div className="capitalize font-fk-display text-[546px] tracking-tighter text-white leading-none  flex flex-col  items-center overflow-hidden -mb-40 mt-24">
+    <div className="capitalize font-fk-display text-[121px] xl:text-[546px] tracking-tighter text-white leading-none  flex flex-col  items-center overflow-hidden -mb-10 xl:-mb-40 xl:mt-24">
       <div>unfold</div>
     </div>
   );
