2c60266417182605ee76f9133c892514a2f91d6e
YuvanX <abhivigneshofficial@gmail.com>
2025-06-10 07:19:46 +0530

    fix: Make Footer Responsive

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -7,12 +7,12 @@ export const NavBar = () => {
   const [selectedTab, setSelectedTab] = useState(TABS[0]);
 
   return (
-    <div className="inline-flex bg-white px-5 py-3 text-xl rounded-full text-black fixed bottom-10 left-1/2 -translate-x-1/2 font-fk-display font-medium shadow-lg z-10 items-center">
+    <div className="inline-flex bg-white px-2 py-2 xl:px-5 xl:py-3 text-[11px] xl:text-xl rounded-full text-black bottom-2 fixed xl:bottom-10  left-1/2 -translate-x-1/2 font-fk-display font-medium shadow-lg z-10 items-center">
       {TABS.map((t, i) => (
         <button
           onClick={() => setSelectedTab(t)}
           key={i}
-          className={`relative px-8 py-2.5   cursor-pointer`}
+          className={`relative px-5 py-1.5 xl:px-8 xl:py-2.5 cursor-pointer`}
         >
           {selectedTab === t && (
             <motion.div layoutId="current-tab" className="absolute inset-0 rounded-full bg-black" />
