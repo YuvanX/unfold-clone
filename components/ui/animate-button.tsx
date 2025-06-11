1619ef0f2a5ee227735d2a09cd1c1da3b043404e
YuvanX <abhivigneshofficial@gmail.com>
2025-06-11 21:12:03 +0530

    Make SafePal Section Responsive md

--- components/ui/animate-button.tsx
+++ components/ui/animate-button.tsx
@@ -3,8 +3,8 @@ import { motion } from "motion/react";
 
 export const CustomButton = () => {
   return (
-    <div className="flex items-center gap-x-1 xl:gap-x-5 bg-white px-3 py-1 xl:px-13 xl:py-4 text-black rounded-full cursor-pointer">
-      <div className="text-sm xl:text-2xl font-fk-grotesk">View</div>
+    <div className="flex items-center gap-x-1  bg-white px-3 py-1 text-black rounded-full cursor-pointer md:py-2 md:px-7 md:gap-x-3 xl:px-13 xl:py-4 xl:gap-x-5">
+      <div className="text-sm md:text-[12px] xl:text-2xl font-fk-grotesk">View</div>
       <motion.svg
         initial={{opacity: 0, x: -5}}
         animate={{opacity: 1, x: 0}}
@@ -13,7 +13,7 @@ export const CustomButton = () => {
         viewBox="0 0 106 82"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
-        className="w-2 xl:w-8"
+        className="w-2 md:w-4 xl:w-8"
       >
         <path
           d="M65.0781 81.0233C65.0781 58.9191 82.9972 41 105.101 41"
--- components/ui/safe-pal.tsx
+++ components/ui/safe-pal.tsx
@@ -2,38 +2,38 @@ import { CustomButton } from "./animate-button";
 
 export const SafePal = () => {
   return (
-    <div className="px-4 xl:min-h-screen xl:px-20 my-10">
-      <div className="relative bg-black rounded-xl  p-2 xl:p-10 xl:rounded-[40px]">
-        <div className="text-4xl font-fk-display text-white xl:text-9xl">
+    <div className="px-4 my-10 md:my-5 md:px-8 xl:min-h-screen xl:px-20">
+      <div className="relative bg-black rounded-xl  p-2 md:p-4 md:rounded-4xl xl:p-10 xl:rounded-[40px]">
+        <div className="text-4xl font-fk-display text-white md:text-[68px] xl:text-9xl">
           Safe Pal
         </div>
         <div className="max-w-[200px] text-[16px] text-white font-semibold font-fk-grotesk md:max-w-full xl:text-xl">
           Making your crypto experiences safe and fun.
         </div>
-        <div className="flex gap-x-2 h-full items-center mt-2 xl:mt-10 xl:gap-x-6 ">
+        <div className="flex gap-x-2 h-full items-center mt-2 md:gap-x-4 xl:mt-10 xl:gap-x-6 ">
           <div className="min-w-[70%] xl:min-w-[50%]">
             <img
               src="https://cdn.prod.website-files.com/63dffdbf1693d14a2308d481/66566f547d37d898e17b77c7_safepal.png"
               alt="safe pal"
-              className="rounded-lg w-full xl:rounded-4xl "
+              className="rounded-lg w-full md:rounded-4xl "
             />
           </div>
-          <div className="xl:max-w-[15%] h-[150px] xl:h-[605px]">
+          <div className=" h-[150px] md:h-[300px]  xl:h-[605px] md:max-w-[15%]">
             <img
               src="https://cdn.prod.website-files.com/63dffdbf1693d14a2308d481/665668fdac8cc6dd79a22bdf_solana.png"
-              className="rounded-lg w-full  h-full xl:rounded-4xl  object-cover"
+              className="rounded-lg w-full  h-full md:rounded-3xl xl:rounded-4xl  object-cover"
               alt="solana image"
             />
           </div>
-          <div className="xl:max-w-[15%] h-[150px] xl:h-[605px]">
+          <div className=" h-[150px] md:h-[300px] xl:h-[605px] md:max-w-[15%]">
             <img
               src="https://cdn.prod.website-files.com/63dffdbf1693d14a2308d481/66566f3a57dda519020fda82_lamar.png"
-              className="rounded-lg w-full  h-full xl:rounded-4xl object-cover"
+              className="rounded-lg w-full  h-full md:rounded-3xl xl:rounded-4xl object-cover"
               alt="solana image"
             />
           </div>
         </div>
-        <div className="absolute bottom-5 left-40 xl:bottom-20 xl:left-1/2">
+        <div className="absolute bottom-5 left-40 md:bottom-10 md:left-100 xl:bottom-20 xl:left-1/2">
           <CustomButton />
         </div>
       </div>
