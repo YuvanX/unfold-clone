2385b424615f06bc476701df041cdccab71dfe55
YuvanX <abhivigneshofficial@gmail.com>
2025-06-14 22:34:01 +0530

    fix: Make responsive Footer Section md

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <div className="capitalize font-fk-display text-[121px] xl:text-[546px] tracking-tighter text-white leading-none  flex flex-col  items-center overflow-hidden -mb-8 xl:-mb-40 mt-10 xl:mt-24">
+    <div className="capitalize font-fk-display text-[121px] md:text-[265px] xl:text-[546px] tracking-tighter text-white leading-none  flex flex-col  items-center overflow-hidden -mb-8 md:-mb-20 xl:-mb-40 mt-10 xl:mt-24">
       <div>unfold</div>
     </div>
   );
