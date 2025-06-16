57a14a28b15837180ab55470fffab313f5cc84fd
YuvanX <abhivigneshofficial@gmail.com>
2025-06-16 22:16:59 +0530

    fix: Make Testimonial Responsive

--- components/ui/testimonials.tsx
+++ components/ui/testimonials.tsx
@@ -19,7 +19,7 @@ export const Testimonials = () => {
   const x2 = useTransform(scrollYProgress, [0, 1], [200, -200]);
   const y2 = useTransform(scrollYProgress, [0, 1], [200, -200]);
 
-  const r1 = useTransform(scrollYProgress, [0, 1], [0, 40]);
+  const r1 = useTransform(scrollYProgress, [0, 1], [-40, 40]);
   const r2 = useTransform(scrollYProgress, [0, 1], [0, 30]);
   const r3 = useTransform(scrollYProgress, [0, 1], [0, -40]);
   const r4 = useTransform(scrollYProgress, [0, 1], [0, -50]);
@@ -32,125 +32,153 @@ export const Testimonials = () => {
   return (
     <div
       ref={ref}
-      className="min-h-screen flex justify-center items-center text-black relative my-20"
+      className="min-h-screen flex justify-center items-center text-black relative mt-20 mb-10 xl:my-20"
     >
-      <motion.div
-        style={{ x: x1, y: y1, rotate: r1 }}
-        className="absolute text-black left-0 top-0"
-      >
-        <Card
-          testimonial={`"In the top 1% of companies I've ever worked with when it comes to people caring about project"`}
-          reviewer="Yuvan from nuvessi"
-          icon={FiActivity}
-        />
-      </motion.div>
+      <div className="hidden xl:block">
+        <motion.div
+          style={{ x: x1, y: y1, rotate: r1 }}
+          className="absolute text-black top-0 left-0"
+        >
+          <Card
+            testimonial={`"In the top 1% of companies I've ever worked with when it comes to people caring about project"`}
+            reviewer="Yuvan from nuvessi"
+            icon={FiActivity}
+          />
+        </motion.div>
 
-      <motion.div
-        style={{ x: x2, y: y2, rotate: r2 }}
-        className="absolute text-black left-0 top-40"
-      >
-        <Image
-          src="https://cdn.prod.website-files.com/63dae19244d432799bf5baa4/63e523b634d60a18bd161b8f_clutch.svg"
-          alt="image-cloud"
-          width={300}
-          height={300}
-        />
-      </motion.div>
+        <motion.div
+          style={{ x: x2, y: y2, rotate: r2 }}
+          className="absolute text-black left-0 top-40"
+        >
+          <Image
+            src="https://cdn.prod.website-files.com/63dae19244d432799bf5baa4/63e523b634d60a18bd161b8f_clutch.svg"
+            alt="image-cloud"
+            width={300}
+            height={300}
+          />
+        </motion.div>
 
-      <motion.div
-        style={{ x: x1, y: y2, rotate: r3 }}
-        className="absolute left-0"
-      >
-        <Card
-          testimonial={`"Unfold's done in a month and a half what we've been trying do in the last year and a half!"`}
-          reviewer="Isha from nuvessi"
-          icon={FiActivity}
-        />
-      </motion.div>
+        <motion.div
+          style={{ x: x1, y: y2, rotate: r3 }}
+          className="absolute left-0"
+        >
+          <Card
+            testimonial={`"Unfold's done in a month and a half what we've been trying do in the last year and a half!"`}
+            reviewer="Isha from nuvessi"
+            icon={FiActivity}
+          />
+        </motion.div>
 
-      <motion.div
-        style={{ x: x2, y: y1, rotate: r4 }}
-        className="absolute left-0 bottom-0"
-      >
-        <Image
-          src="https://cdn.prod.website-files.com/63dae19244d432799bf5baa4/63e525277e6bfc538276cd57_chat.svg"
-          alt="image-chat"
-          width={300}
-          height={300}
-        />
-      </motion.div>
+        <motion.div
+          style={{ x: x2, y: y1, rotate: r4 }}
+          className="absolute left-0 bottom-0"
+        >
+          <Image
+            src="https://cdn.prod.website-files.com/63dae19244d432799bf5baa4/63e525277e6bfc538276cd57_chat.svg"
+            alt="image-chat"
+            width={300}
+            height={300}
+          />
+        </motion.div>
 
-      <motion.div
-        style={{ x: x2, y: y1, rotate: r5 }}
-        className="absolute right-10 top-0"
-      >
-        <Card
-          testimonial={`"When the Data Scientists are saying the UX/UI looks good... that's a BIG deal(rarely happens). 👏"`}
-          reviewer="Abhi from nuvessi"
-          icon={FiActivity}
-        />
-      </motion.div>
+        <motion.div
+          style={{ x: x2, y: y1, rotate: r5 }}
+          className="absolute right-10 top-0"
+        >
+          <Card
+            testimonial={`"When the Data Scientists are saying the UX/UI looks good... that's a BIG deal(rarely happens). 👏"`}
+            reviewer="Abhi from nuvessi"
+            icon={FiActivity}
+          />
+        </motion.div>
 
-      <motion.div
-        style={{ x: x1, y: y2, rotate: r6 }}
-        className="absolute right-0"
-      >
-        <Image
-          src="https://cdn.prod.website-files.com/63dae19244d432799bf5baa4/63e524b35f5d677e9845c642_pencil.svg"
-          width={300}
-          height={300}
-          alt="image-pencil"
-        />
-      </motion.div>
+        <motion.div
+          style={{ x: x1, y: y2, rotate: r6 }}
+          className="absolute right-0"
+        >
+          <Image
+            src="https://cdn.prod.website-files.com/63dae19244d432799bf5baa4/63e524b35f5d677e9845c642_pencil.svg"
+            width={300}
+            height={300}
+            alt="image-pencil"
+          />
+        </motion.div>
 
-      <motion.div
-        style={{ x: x1, y: y2, rotate: r7 }}
-        className="absolute right-0"
-      >
-        <Card
-          testimonial={`"Huge thanks to the unfold team. Really proud of this one"`}
-          reviewer="Abhi from nuvessi"
-          icon={FiActivity}
-        />
-      </motion.div>
+        <motion.div
+          style={{ x: x1, y: y2, rotate: r7 }}
+          className="absolute right-0"
+        >
+          <Card
+            testimonial={`"Huge thanks to the unfold team. Really proud of this one"`}
+            reviewer="Abhi from nuvessi"
+            icon={FiActivity}
+          />
+        </motion.div>
 
-      <motion.div style={{ x: x2, y: y1, rotate: r8 }} className="absolute bottom-50 right-0">
-        <Image
-          src="https://cdn.prod.website-files.com/63dae19244d432799bf5baa4/63e526efa973a610da0df9e8_farley.gif"
-          width={100}
-          height={100}
-          alt="image-pfp"
-        />
-      </motion.div>
+        <motion.div
+          style={{ x: x2, y: y1, rotate: r8 }}
+          className="xl:absolute xl:bottom-50 xl:right-0"
+        >
+          <Image
+            src="https://cdn.prod.website-files.com/63dae19244d432799bf5baa4/63e526efa973a610da0df9e8_farley.gif"
+            width={100}
+            height={100}
+            alt="image-pfp"
+          />
+        </motion.div>
 
-      <motion.div
-        style={{ x: x1, y: y2, rotate: r9 }}
-        className="absolute right-0 bottom-0"
-      >
-        <Card
-          testimonial={`"Your designers bring so many mind-opemning ideas yet tangible designs to the project. I'm very impressed"`}
-          reviewer="Alex from nuvessi"
-          icon={FiActivity}
-        />
-      </motion.div>
+        <motion.div
+          style={{ x: x1, y: y2, rotate: r9 }}
+          className="absolute right-0 bottom-0"
+        >
+          <Card
+            testimonial={`"Your designers bring so many mind-opemning ideas yet tangible designs to the project. I'm very impressed"`}
+            reviewer="Alex from nuvessi"
+            icon={FiActivity}
+          />
+        </motion.div>
+      </div>
 
-      
+
       <div className="flex flex-col items-center ">
         <div className="gap-x-2 px-5 py-2 bg-white rounded-full flex items-center">
-          <span className="text-[18px] font-fk-grotesk">
+          <span className="text-[14px] xl:text-[16px] font-fk-grotesk">
             several people are typing
           </span>
           <LoadingAnimation />
         </div>
 
-        <div className="text-[99px] font-fk-display text-center leading-25 max-w-5xl mt-4">
+        <div className="text-[25px]/6 mx-10 xl:mx-0 xl:text-[99px] font-fk-display text-center xl:leading-25 max-w-5xl mt-4">
           Our Clients Can’t Say Enough About Us.
         </div>
 
-        <div className="font-fk-grotesk text-[18px] max-w-md text-center mt-5">
+        <div className="font-fk-grotesk mx-10 xl:mx-0 text-[16px]/5 xl:text-[18px] max-w-md text-center mt-5">
           Take a look for yourself. No Slack messages were harmed in the making
           of this component.
         </div>
+
+        <div className="flex flex-col xl:hidden mt-8">
+          <Card
+            testimonial={`"In the top 1% of companies I've ever worked with when it comes to people caring about project"`}
+            reviewer="Yuvan from nuvessi"
+            icon={FiActivity}
+          />
+          <Card
+            testimonial={`"Unfold's done in a month and a half what we've been trying do in the last year and a half!"`}
+            reviewer="Isha from nuvessi"
+            icon={FiActivity}
+          />
+          <Card
+            testimonial={`"When the Data Scientists are saying the UX/UI looks good... that's a BIG deal(rarely happens). 👏"`}
+            reviewer="Abhi from nuvessi"
+            icon={FiActivity}
+          />
+         <Card
+            testimonial={`"Huge thanks to the unfold team. Really proud of this one"`}
+            reviewer="Abhi from nuvessi"
+            icon={FiActivity}
+          />
+      </div>
       </div>
     </div>
   );
@@ -167,11 +195,11 @@ const Card = ({
 }) => {
   const Icon = icon;
   return (
-    <div className="flex flex-col gap-y-2 rounded-4xl bg-white text-black font-fk-grotesk px-10 py-8 max-w-xl">
-      <div className="font-fk-grotesk text-xl font-semibold">{testimonial}</div>
-      <div className="flex items-center gap-x-2">
+    <div className="flex flex-col gap-y-5 xl:gap-y-2 mx-5 my-2 rounded-lg xl:rounded-4xl bg-white text-black font-fk-grotesk px-4 py-3 xl:px-10 xl:py-8 max-w-xl">
+      <div className="font-fk-grotesk text-[16px] xl:text-xl xl:font-semibold">{testimonial}</div>
+      <div className="flex flex-col  xl:flex-row xl:items-center gap-x-2">
         <Icon />
-        <div>{reviewer}</div>
+        <div className="text-sm">{reviewer}</div>
       </div>
     </div>
   );
