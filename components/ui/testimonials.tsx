f5dcfc8eaf16b3c2835def71426a1d2e23e38b37
YuvanX <abhivigneshofficial@gmail.com>
2025-06-17 15:20:56 +0530

    fix: Complete testimonailSection

--- components/ui/testimonials.tsx
+++ components/ui/testimonials.tsx
@@ -3,8 +3,6 @@
 import { useRef } from "react";
 import { LoadingAnimation } from "./loading-animation";
 import { useScroll, motion, useTransform } from "motion/react";
-import { FiActivity } from "react-icons/fi";
-import { IconType } from "react-icons";
 import Image from "next/image";
 
 export const Testimonials = () => {
@@ -14,25 +12,46 @@ export const Testimonials = () => {
     offset: ["start end", "end start"],
   });
 
-  const x1 = useTransform(scrollYProgress, [0, 1], [200, -200]);
-  const y1 = useTransform(scrollYProgress, [0, 1], [200, -200]);
-  const x2 = useTransform(scrollYProgress, [0, 1], [200, -200]);
+  const x1 = useTransform(scrollYProgress, [0, 1], [500, -100]);
+  const y1 = useTransform(scrollYProgress, [0, 1], [200, -100]);
+
+  const x2 = useTransform(scrollYProgress, [0, 1], [100, -100]);
   const y2 = useTransform(scrollYProgress, [0, 1], [200, -200]);
 
-  const r1 = useTransform(scrollYProgress, [0, 1], [-40, 40]);
-  const r2 = useTransform(scrollYProgress, [0, 1], [0, 30]);
-  const r3 = useTransform(scrollYProgress, [0, 1], [0, -40]);
-  const r4 = useTransform(scrollYProgress, [0, 1], [0, -50]);
-  const r5 = useTransform(scrollYProgress, [0, 1], [0, -40]);
-  const r6 = useTransform(scrollYProgress, [0, 1], [0, -60]);
-  const r7 = useTransform(scrollYProgress, [0, 1], [0, -20]);
-  const r8 = useTransform(scrollYProgress, [0, 1], [0, -10]);
+  const x3 = useTransform(scrollYProgress, [0, 1], [300, -50]);
+  const y3 = useTransform(scrollYProgress, [0, 1], [-50, 200]);
+
+  const y4 = useTransform(scrollYProgress, [0, 1], [50, 200]);
+
+  const x5 = useTransform(scrollYProgress, [0, 1], [-400, 100]);
+  const y5 = useTransform(scrollYProgress, [0, 1], [250, -50]);
+
+  const x6 = useTransform(scrollYProgress, [0, 1], [-400, 200])
+ 
+  const x7 = useTransform(scrollYProgress, [0, 1], [-400, 100]);
+  const y7 = useTransform(scrollYProgress, [0, 1], [200, 200]);
+
+  const x8 = useTransform(scrollYProgress, [0, 1], [-100, 10]);
+  const y8 = useTransform(scrollYProgress, [0, 1], [100, 200]);
+
+  const x9 = useTransform(scrollYProgress, [0, 1], [100, 200]);
+  const y9 = useTransform(scrollYProgress, [0, 1], [-200, 100]);
+
+  const r1 = useTransform(scrollYProgress, [0, 1], [-40, 60]);
+  const r2 = useTransform(scrollYProgress, [0, 1], [-60, 10]);
+  const r3 = useTransform(scrollYProgress, [0, 1], [-40, 40]);
+  const r4 = useTransform(scrollYProgress, [0, 1], [-40, 0]);
+  const r5 = useTransform(scrollYProgress, [0, 1], [50, -40]);
+  const r6 = useTransform(scrollYProgress, [0, 1], [20, -30]);
+  const r7 = useTransform(scrollYProgress, [0, 1], [0, 30]);
+  const r8 = useTransform(scrollYProgress, [0, 1], [0, 30]);
   const r9 = useTransform(scrollYProgress, [0, 1], [0, -10]);
-  const r10 = useTransform(scrollYProgress, [0, 1], [0, -5]);
+
+
   return (
     <div
       ref={ref}
-      className="min-h-screen flex justify-center items-center text-black relative mt-20 mb-10 xl:my-20"
+      className="min-h-screen flex justify-center items-center text-black relative mt-20 mb-10 xl:mt-40 xl:mb-80"
     >
       <div className="hidden xl:block">
         <motion.div
@@ -48,7 +67,7 @@ export const Testimonials = () => {
 
         <motion.div
           style={{ x: x2, y: y2, rotate: r2 }}
-          className="absolute text-black left-0 top-40"
+          className="absolute text-black  top-70 left-10"
         >
           <Image
             src="https://cdn.prod.website-files.com/63dae19244d432799bf5baa4/63e523b634d60a18bd161b8f_clutch.svg"
@@ -59,8 +78,8 @@ export const Testimonials = () => {
         </motion.div>
 
         <motion.div
-          style={{ x: x1, y: y2, rotate: r3 }}
-          className="absolute left-0"
+          style={{ x: x3, y: y3, rotate: r3 }}
+          className="absolute left-0 bottom-50"
         >
           <Card
             testimonial={`"Unfold's done in a month and a half what we've been trying do in the last year and a half!"`}
@@ -70,19 +89,19 @@ export const Testimonials = () => {
         </motion.div>
 
         <motion.div
-          style={{ x: x2, y: y1, rotate: r4 }}
-          className="absolute left-0 bottom-0"
+          style={{ y: y4, rotate: r4 }}
+          className="absolute left-20 bottom-0"
         >
           <Image
             src="https://cdn.prod.website-files.com/63dae19244d432799bf5baa4/63e525277e6bfc538276cd57_chat.svg"
             alt="image-chat"
-            width={300}
-            height={300}
+            width={220}
+            height={220}
           />
         </motion.div>
 
         <motion.div
-          style={{ x: x2, y: y1, rotate: r5 }}
+          style={{ x: x5, y: y5, rotate: r5 }}
           className="absolute right-10 top-0"
         >
           <Card
@@ -93,19 +112,19 @@ export const Testimonials = () => {
         </motion.div>
 
         <motion.div
-          style={{ x: x1, y: y2, rotate: r6 }}
-          className="absolute right-0"
+          style={{ x: x6, rotate: r6 }}
+          className="absolute right-20 top-80"
         >
           <Image
             src="https://cdn.prod.website-files.com/63dae19244d432799bf5baa4/63e524b35f5d677e9845c642_pencil.svg"
-            width={300}
-            height={300}
+            width={200}
+            height={200}
             alt="image-pencil"
           />
         </motion.div>
 
         <motion.div
-          style={{ x: x1, y: y2, rotate: r7 }}
+          style={{ x: x7, y: y7, rotate: r7 }}
           className="absolute right-0"
         >
           <Card
@@ -116,20 +135,21 @@ export const Testimonials = () => {
         </motion.div>
 
         <motion.div
-          style={{ x: x2, y: y1, rotate: r8 }}
-          className="xl:absolute xl:bottom-50 xl:right-0"
+          style={{ x: x8, y: y8, rotate: r8 }}
+          className="absolute right-50 bottom-30"
         >
           <Image
             src="https://cdn.prod.website-files.com/63dae19244d432799bf5baa4/63e526efa973a610da0df9e8_farley.gif"
-            width={100}
-            height={100}
+            width={120}
+            height={120}
             alt="image-pfp"
+            className="rounded-lg"
           />
         </motion.div>
 
         <motion.div
-          style={{ x: x1, y: y2, rotate: r9 }}
-          className="absolute right-0 bottom-0"
+          style={{ x: x9, y: y9, rotate: r9 }}
+          className="absolute right-1/2 translate-1/2 bottom-0"
         >
           <Card
             testimonial={`"Your designers bring so many mind-opemning ideas yet tangible designs to the project. I'm very impressed"`}
@@ -159,24 +179,24 @@ export const Testimonials = () => {
 
         <div className="flex flex-col xl:hidden mt-8">
           <Card
-            testimonial={`"In the top 1% of companies I've ever worked with when it comes to people caring about project"`}
-            reviewer="Yuvan from nuvessi"
+            testimonial={`"Your designers bring so many mind-opemning ideas yet tangible designs to the project. I'm very impressed"`}
+            reviewer="Alex from nuvessi"
             icon="https://cdn.prod.website-files.com/63dae19244d432799bf5baa4/665a018147f26450b8d1c326_image_2024-05-31_12-36-23.png"
           />
           <Card
+            testimonial={`"Huge thanks to the unfold team. Really proud of this one"`}
+            reviewer="Abhi from nuvessi"
+            icon="https://cdn.prod.website-files.com/63dae19244d432799bf5baa4/665a01757508c661ca1da843_image_2024-05-31_12-35-52.png"
+          />
+           <Card
             testimonial={`"Unfold's done in a month and a half what we've been trying do in the last year and a half!"`}
             reviewer="Isha from nuvessi"
-            icon="https://cdn.prod.website-files.com/63dae19244d432799bf5baa4/665a018147f26450b8d1c326_image_2024-05-31_12-36-23.png"
-          />
-          <Card
-            testimonial={`"When the Data Scientists are saying the UX/UI looks good... that's a BIG deal(rarely happens). 👏"`}
-            reviewer="Abhi from nuvessi"
-            icon="https://cdn.prod.website-files.com/63dae19244d432799bf5baa4/665a018147f26450b8d1c326_image_2024-05-31_12-36-23.png"
+            icon="https://cdn.prod.website-files.com/63dae19244d432799bf5baa4/665a01571b3134a64425d44a_image_2024-05-31_12-35-12-p-500.png"
           />
          <Card
             testimonial={`"Huge thanks to the unfold team. Really proud of this one"`}
-            reviewer="Honey from nuvessi"
-            icon="https://cdn.prod.website-files.com/63dae19244d432799bf5baa4/665a018147f26450b8d1c326_image_2024-05-31_12-36-23.png"
+            reviewer="Abhi from nuvessi"
+            icon="https://cdn.prod.website-files.com/63dae19244d432799bf5baa4/665a01757508c661ca1da843_image_2024-05-31_12-35-52.png"
           />
       </div>
       </div>
